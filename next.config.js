b2770401e56ef41b043de95c88c9b1cc91c095f0
kleenkanteen <antpalmolive@gmail.com>
2023-06-06 18:18:43 -0400

    point to railway backend

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "dtogetherca-backend-production.up.railway.app", "localhost"],
   },
 })
 

45844adbab85a880f1a4fe3ab98b1cf4cf1d4d68
kleenkanteen <antpalmolive@gmail.com>
2023-06-11 13:51:41 -0400

    change homepage text

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="h-[96vh] w-full relative">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32">
         <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          A marketplace for diabetics to sell excess supplies. See all products:
+          A marketplace for diabetics to sell excess supplies to each other.
         </h1>
         <UnderlineLink href="/store">Explore products</UnderlineLink>
       </div>

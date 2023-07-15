c7f4dd9818fc2930f2010620503bbfe6f37810a3
kleenkanteen <antpalmolive@gmail.com>
2023-07-15 16:58:12 -0400

    fix s3 url

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["https://medusa-dtogether.s3.amazonaws.com", "dtogetherca-backend-production.up.railway.app", "medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: ["medusa-dtogether.s3.us-east-2.amazonaws.com", "dtogetherca-backend-production.up.railway.app", "medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
     // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     // imageSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
